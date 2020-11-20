eaaf1670f5d22cd2cffb5f39462420bec76a0258
SayBeen <clubin30@gmail.com>
2020-11-20 11:49:57 +0900

    Add community & accounts

--- final_project/urls.py
+++ final_project/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('community/', include('community.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
