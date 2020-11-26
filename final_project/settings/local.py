6a7c475172098b1b4b40ec825e0a43a51f8ca9c0
unknown <fcduck16@gmail.com>
2020-11-26 13:49:26 +0900

    Ready to deploy

---
+++ final_project/settings/local.py
@@ -0,0 +1,9 @@
+# settings/local.py
+
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '3.34.131.205',
+]
---
+++ final_project/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+ # lightsail public_ip 주소
+ # ex) 13.126.105.123
+]
