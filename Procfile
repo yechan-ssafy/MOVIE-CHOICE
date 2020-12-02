415db82dbb3ff7cffc4961d8394a0f07c0ffd6b9
unknown <fcduck16@gmail.com>
2020-12-02 16:57:37 +0900

    Fix settings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn final_project.wsgi --log-file -
+web: gunicorn final_project.wsgi
--- final_project/settings/local.py
+++ final_project/settings/local.py
@@ -4,6 +4,4 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'movie-choice-app.herokuapp.com',
-]
+ALLOWED_HOSTS = []
