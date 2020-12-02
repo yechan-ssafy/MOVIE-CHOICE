f3577750b3827dd459297189d3d2e7b3d59815f1
unknown <fcduck16@gmail.com>
2020-12-02 14:09:09 +0900

    Ready to herokuDeploy

--- final_project/settings/base.py
+++ final_project/settings/base.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,6 +151,13 @@ STATICFILES_DIRS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
+
+# Heroku Deploy
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # # Message Framework
 # from django.contrib.messages import constants as messages_constants
